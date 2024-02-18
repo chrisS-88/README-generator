702f3881045307caf064b333b7c3369ce38ee9af
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-02-18 19:59:35 +0000

    files added and questions set

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please add a brief description of your project.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please add usage instructions.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license would you like to add?",
+    choices: ["N/A", "MIT", "Apache 2.0", "GNU v3.0", "Boost 1.0", "GNU v2.0"],
+  },
+  {
+    type: "input",
+    name: "contribition",
+    message: "Please enter contribution guidelines for your project",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please enter a tests guide for your project",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions);
 }
 
 // function call to initialize program
